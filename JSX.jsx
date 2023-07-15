c35da662239c00c1357739c4ccc3f604fe5120dd
sundararajan-git <121149600+sundararajan-git@users.noreply.github.com>
2023-07-15 10:14:08 +0530

    Create JSX.jsx

---
+++ JSX.jsx
@@ -0,0 +1,17 @@
+<>
+  //--- className
+  <div className="JSX">
+    <p>JSX stands for JavaScript XML.</p>
+    <p>JSX allows us to write HTML in React.</p>
+    <p>JSX makes it easier to write and add HTML in React.</p>
+  </div>
+  // --------htmlfor
+  <div className="input">
+    <label htmlFor="name">Name</label>
+    <input type="text" id="name" placeholder="Your Name" />
+  </div>
+  //------------ experstions 
+  <p>React is {5 + 5} times better with JSX</p>
+  // --------conditions ( if , logical ,ternary )
+  {true && <p>Hello world</p>}
+</>;
